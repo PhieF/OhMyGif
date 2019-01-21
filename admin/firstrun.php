f74467781eca871605ec58bf7323cc587e956cd8
PhieF <phie@phie.ovh>
2019-01-21 15:17:46 +0100

    Update firstrun.php

--- admin/firstrun.php
+++ admin/firstrun.php
@@ -23,7 +23,7 @@ and !empty($_POST['password'])){
 		video VARCHAR(255) UNIQUE,
 		original_name VARCHAR(255) NOT NULL,
 		description TEXT NOT NULL,
-		category VARCHAR(100)
+		category VARCHAR(100),
 		title VARCHAR(255),
 		original_url VARCHAR(255),
 		date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
